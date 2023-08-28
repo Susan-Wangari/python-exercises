a69261a1e76e84ab7494e3adf002a3df682b8ec1
Susan Wangari <56164343+Susan-Wangari@users.noreply.github.com>
2023-08-28 21:16:44 +0300

    Create findReplace.py

---
+++ findReplace.py
@@ -0,0 +1,11 @@
+msg = 'welcome to Kenya'
+
+//find
+print(msg.find("l"))
+
+//replace
+print(msg.replace("Kenya", "Nairobi"))
+
+//membership
+//checks if python exists in the message variable
+print('python' in message)
