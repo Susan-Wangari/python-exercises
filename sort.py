663239c212986a1cfc6c69aa7ebdf238150f81df
Susan Wangari <56164343+Susan-Wangari@users.noreply.github.com>
2023-11-16 22:15:10 +0300

    Create sort.py

---
+++ sort.py
@@ -0,0 +1,11 @@
+myList = [9,7,5,8,6]
+myList.sort()
+print(myList) 
+//prints [5,6,7,8,9] // it is in ascending order
+
+fruits = ['z', 'd', 'c', 'k']
+fruits.sort()
+print(fruits)
+
+//it prints the fruits arranged in alphabetical order
+          
