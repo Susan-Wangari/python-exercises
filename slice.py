a5edfefa3d8bcad8d3b16d7ff4cac35128044ffb
Susan Wangari <56164343+Susan-Wangari@users.noreply.github.com>
2023-11-30 21:10:26 +0300

    Create a sliced list in python

---
+++ slice.py
@@ -0,0 +1,5 @@
+//slicing in python
+//task1:extracttext and create an ew strig that says '1 Welcome Ring To Tyler' every first letter in a word should be capitalized 
+
+msg= 'Welcome to Python 101: Strings'
+newMsg = msg[18] + msg[:8] + msg[25:29] + msg[8:11]+ msg[13:12] + msg[2:6] + msg[-5]
