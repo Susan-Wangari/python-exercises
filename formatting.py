c7ab898a712333297fd666dad82db43b02b7fe8a
Susan Wangari <56164343+Susan-Wangari@users.noreply.github.com>
2023-11-30 22:07:45 +0300

    Create string formatting in python

---
+++ formatting.py
@@ -0,0 +1,7 @@
+#string formatting helps you avoid using + sign in your code and make it more readable
+name = 'Wan'
+age = 12
+details = 'name' + ' ' + 'is' + ' ' + 'years old'
+
+/#formatted string 
+  details = f '{name} is {age} years old'
